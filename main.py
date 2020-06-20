a672e39332c32d466386f6918c9749629803f439
BennySpaceman <daniil.vlasov.02@mail.ru>
2020-06-21 00:36:43 +0300

    commit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import discord
-import youtube_dl
 import os
+import discord
+import asyncio
+import youtube_dl
 import functions as f
-from discord.ext import commands
 from discord.utils import get
+from discord.ext import commands
 
 PREFIX = '-'
 bot = commands.Bot(command_prefix = PREFIX)
@@ -12,7 +13,7 @@ bot.remove_command('help')
 
 @bot.event
 async def on_ready():
-    print('BOT connected')
+    print('Bot is ready to rock!')
 
     await bot.change_presence(status = discord.Status.online, 
                               activity = discord.Game('D&D 5e | -help'))
@@ -33,29 +34,77 @@ async def clear(ctx, amount = 100):
     await ctx.channel.purge(limit = amount)  
 
 
-@bot.command()
-async def join(ctx):
-    global voice
+# @bot.command()
+# async def join(ctx):
+#     global voice 
+#     channel = ctx.message.author.voice.channel
+#     voice = get(bot.voice_clients, guild = ctx.guild)
+
+#     if voice and voice.is_connected():
+#         await voice.move_to(channel)
+#     else:
+#         voice = await channel.connect()
+
+
+# @bot.command()
+# async def leave(ctx):
+#     channel = ctx.message.author.voice.channel
+#     voice = get(bot.voice_clients, guild = ctx.guild)
+
+#     if voice and voice.is_connected():
+#         await voice.disconnect()
+#     else:
+#         voice = await channel.connect()
+
+
+@bot.command(pass_context=True)
+async def YoYoPiraka(ctx):
     channel = ctx.message.author.voice.channel
     voice = get(bot.voice_clients, guild = ctx.guild)
+    voice = await channel.connect()
+    voice.play(discord.FFmpegPCMAudio('Piraka Rap.mp3'))
+        
 
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else: 
-        voice = await channel.connect()
-        await ctx.send(f'Bot connected to the channel: {channel}')
+# @bot.command()
+# async def play(ctx, url: str):
+#     song_there = os.path.isfile('song.mp3')
 
+#     try:
+#         if song_there:
+#             os.remove('song.mp3')
+#             print('[log] Olf dile deleted')
+#     except PermissionError:
+#         print('[log] Failure to download file')
+    
+#     await ctx.send('Please, standby')
 
-@bot.command()
-async def leave(ctx):
-    channel = ctx.message.author.voice.channel
-    voice = get(bot.voice_clients, guild = ctx.guild)
+#     voice = get(bot.voice_clients, guild = ctx.guild)
 
-    if voice and voice.is_connected():
-        await voice.disconnect()
-    else: 
-        voice = await channel.connect()
-        await ctx.send(f'Bot disconnected from the channel: {channel}')
+#     ydl_opts = {
+#         'format': 'bestaudio/best',
+#         'postprocessors': [{
+#             'key': 'FFmpegExtractAudio',
+#             'preferredcodec': 'mp3', 
+#             'preferredquality': '192'
+#         }],
+#     }
+
+#     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#         print('[log] Downloading music...')
+#         ydl.download([url])
+
+#     for file in os.listdir('./'):
+#         if file.endswith('.mp3'):
+#             name = file
+#             print('[log] Renaming file: {file}')
+#             os.rename(file, 'song.mp3')
+
+#     voice.play(discord.FFmpegPCMAudio('song.mp3'), after = lambda e: print(f'[log] {name}, music ended'))
+#     voice.source = discord.PCMVolumeTransformer(voice.source)
+#     voice.source.volume = 0.07
+
+#     song_name = name.rsplit('-', 2)
+#     await ctx.send(f'Now playing: {song_name[0]}')
 
 
 @bot.command()
