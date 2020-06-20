ac3ec33c7f360562dd4de375a0efc393c8b2416f
DanilaOl <danilaolyanin@yandex.ru>
2020-06-20 16:50:29 +0300

    beta 1.0

--- functions.py
+++ functions.py
@@ -1,13 +1,15 @@
 import random
+
 ALLOWED_EDGES = (4, 6, 8, 10, 12, 20)
 
 
 def roll(dice):
+    # This function generates random numbers in one of allowed ranges.
+    # It takes string in xdy format, where x is quantity of rolls and y is dice edges amount
     rolls_qnt, edges = dice.split('d')
     rolls_qnt, edges = int(rolls_qnt), int(edges)
-
-    if rolls_qnt not in ALLOWED_EDGES:
-        return 'Ty Invalid, Net Takogo Kubika'
+    if edges not in ALLOWED_EDGES:
+        return 'Ты инвалид, нет такого кубика'
 
     elif rolls_qnt == 1:
         score = random.randint(1, edges)
@@ -26,14 +28,33 @@ def roll(dice):
 
 
 def show_score(dice):
-    all_scores, final_score = roll(dice)
-    final_str = ' + '.join(str(i) for i in all_scores) + ' = ' + str(final_score)
-    return final_str
+    # Prettifying output of roll function
+    if int(dice.split('d')[0]) == 1:
+        return roll(dice)
+    else:
+        all_scores, final_score = roll(dice)
+        output = ' + '.join(str(i) for i in all_scores) + ' = ' + str(final_score)
+        return output
 
 
 def rand_params():
+    # Generates random parameters for your character's stats
     params = []
     for i in range(6):
         scores = sorted(roll("4d6")[0])[1:]
         params.append(sum(scores))
-    return params
+    return str(params)
+
+
+def advantage_roll(dice):
+    # Makes 2 rolls and choosing greatest
+    rolls = [roll(dice), roll(dice)]
+    output = " или ".join(rolls) + f". Итого {max(rolls)}"
+    return output
+
+
+def disadvantage_roll(dice):
+    # Makes 2 rolls and choosing lowest
+    rolls = [roll(dice), roll(dice)]
+    output = " или ".join(rolls) + f". Итого {min(rolls)}"
+    return output
