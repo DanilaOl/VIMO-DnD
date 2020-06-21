be0ef1ac1383d0cf4b0a25ad56781903353d89cd
BennySpaceman <daniil.vlasov.02@mail.ru>
2020-06-21 16:16:57 +0300

    commit

--- main.py
+++ main.py
@@ -3,65 +3,66 @@ import discord
 import asyncio
 import youtube_dl
 import functions as f
+from time import sleep
 from discord.utils import get
 from discord.ext import commands
 
 PREFIX = '-'
-bot = commands.Bot(command_prefix = PREFIX)
+bot = commands.Bot(command_prefix=PREFIX)
 bot.remove_command('help')
 
 
-#Checking for connection and setting up bot status
+# Checking for connection and setting up bot status
 @bot.event
 async def on_ready():
     print('Bot is ready to rock!')
 
-    await bot.change_presence(status = discord.Status.online, 
-                              activity = discord.Game('D&D 5e | -help'))
+    await bot.change_presence(status=discord.Status.online, 
+                              activity=discord.Game('D&D 5e | -help'))
 
 
-#List of bot's commands
-@bot.command(pass_context = True)
+# List of bot's commands
+@bot.command(pass_context=True)
 async def help(ctx):
-    emb = discord.Embed(title = 'Commands list')
+    emb = discord.Embed(title='Commands list')
     
-    emb.add_field(name = '{}clear'.format(PREFIX), value = 'Clearing chat')
+    emb.add_field(name='{}clear'.format(PREFIX), value='Clearing chat')
 
-    await ctx.send(embed = emb)
-
-
-#Clears last 100 messages
-@bot.command(pass_content = True)
-@commands.has_permissions(administrator = True)
-async def clear(ctx, amount = 100):
-    await ctx.channel.purge(limit = amount)  
+    await ctx.send(embed=emb)
 
 
-#Kicks user
-@bot.command(pass_content = True)
-@commands.has_permissions(administrator = True)
-async def kick(ctx, member: discord.Member, *, reason = None):
-    await ctx.channel.purge(limit = 1)
+# Clears last 100 messages
+@bot.command(pass_content=True)
+@commands.has_permissions(administrator=True)
+async def clear(ctx, amount=100):
+    await ctx.channel.purge(limit=amount)  
+
+
+# Kicks user
+@bot.command(pass_content=True)
+@commands.has_permissions(administrator=True)
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await ctx.channel.purge(limit=1)
     
-    await member.kick(reason = reason)
+    await member.kick(reason=reason)
     await ctx.send(f'User kicked {member.mention}')
 
 
-#Bans user
-@bot.command(pass_content = True)
-@commands.has_permissions(administrator = True)
-async def ban(ctx, member: discord.Member, *, reason = None):
+# Bans user
+@bot.command(pass_content=True)
+@commands.has_permissions(administrator=True)
+async def ban(ctx, member: discord.Member, *, reason=None):
     await ctx.channel.purge(limit = 1)
     
     await member.ban(reason = reason)
     await ctx.send(f'User banned {member.mention}')
 
 
-#Unbans user
-@bot.command(pass_context = True)
-@commands.has_permissions(administrator = True)
+# Unbans user
+@bot.command(pass_context=True)
+@commands.has_permissions(administrator=True)
 async def unban(ctx, *, member):
-    await ctx.channel.purge(limit = 1)
+    await ctx.channel.purge(limit=1)
 
     banned_users = await ctx.guild.bans()
 
@@ -74,11 +75,11 @@ async def unban(ctx, *, member):
         return 
 
 
-#Leaves voice-chat
+# Leaves voice-chat
 @bot.command()
 async def leave(ctx):
     channel = ctx.message.author.voice.channel
-    voice = get(bot.voice_clients, guild = ctx.guild)
+    voice = get(bot.voice_clients, guild=ctx.guild)
 
     if voice and voice.is_connected():
         await voice.disconnect()
@@ -86,17 +87,17 @@ async def leave(ctx):
         voice = await channel.connect()
 
 
-#The most awesome easter-egg in the world XD
+# The most awesome easter-egg in the world XD
 @bot.command(pass_context=True)
 async def YoYoPiraka(ctx):
     channel = ctx.message.author.voice.channel
-    voice = get(bot.voice_clients, guild = ctx.guild)
+    voice = get(bot.voice_clients, guild=ctx.guild)
     
     voice = await channel.connect()
     voice.play(discord.FFmpegPCMAudio('Piraka Rap.mp3'))
         
 
-#Rolls dices in xdy format
+# Rolls dices in xdy format
 @bot.command()
 async def roll(ctx, arg):
     author = ctx.message.author
@@ -104,7 +105,7 @@ async def roll(ctx, arg):
     await ctx.send(f'{result} {author.mention}')
 
 
-#Rolls dices with advantage
+# Rolls dices with advantage
 @bot.command()
 async def advRoll(ctx, arg):
     author = ctx.message.author
@@ -112,7 +113,7 @@ async def advRoll(ctx, arg):
     await ctx.send(f'{result} {author.mention}')
 
 
-#Rolls dices with disadvantage
+# Rolls dices with disadvantage
 @bot.command()
 async def dadvRoll(ctx, arg):
     author = ctx.message.author
@@ -120,7 +121,7 @@ async def dadvRoll(ctx, arg):
     await ctx.send(f'{result} {author.mention}')
 
 
-#Rolls dices to make a death check
+# Rolls dices to make a death check
 @bot.command()
 async def deathRoll(ctx):
     author = ctx.message.author
@@ -128,7 +129,7 @@ async def deathRoll(ctx):
     await ctx.send(f'{result} {author.mention}')
 
 
-#Flips coin 
+# Flips coin 
 @bot.command()
 async def flipCoin(ctx):
     author = ctx.message.author
@@ -136,7 +137,7 @@ async def flipCoin(ctx):
     await ctx.send(f'{result} {author.mention}')
 
 
-#Generates random parametres for charachter
+#Generates random parameters for character
 @bot.command()
 async def randParams(ctx):
     author = ctx.message.author
