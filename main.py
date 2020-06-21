6849fb7d90c80068fca31f95fd5254e8739e587c
BennySpaceman <daniil.vlasov.02@mail.ru>
2020-06-21 16:25:22 +0300

    commit

--- main.py
+++ main.py
@@ -125,8 +125,26 @@ async def dadvRoll(ctx, arg):
 @bot.command()
 async def deathRoll(ctx):
     author = ctx.message.author
-    result = f.death_roll()
-    await ctx.send(f'{result} {author.mention}')
+    successes, failures = 0, 0
+    await ctx.send(f'Бросаю кубики для {author.mention}')
+
+    while successes < 3 and failures < 3:
+        for i in range(3):
+            sleep(1)
+            await ctx.send('...')
+        sleep(1)
+        if f.flip_coin() == 'Орёл':
+            await ctx.send(f'Успех {author.mention}')
+            successes += 1
+        else:
+            await ctx.send(f'Провал {author.mention}')
+            failures += 1
+    sleep(1)
+    if successes == 3:
+        await ctx.send(f'Поздравляю, ваш персонаж выжил {author.mention}')
+    elif failures == 3:
+        await ctx.send(f'К сожалению, ваш персонаж погиб {author.mention}')
+#       TODO: delete character from file
 
 
 # Flips coin 
