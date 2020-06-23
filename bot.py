0b1e505d2eb7ae9e201abcbf53b68d012c337b1d
BennySpaceman <daniil.vlasov.02@mail.ru>
2020-06-23 23:50:38 +0300

    commit

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ async def on_raw_reaction_add(ctx):
 
         emoji = str(ctx.emoji)
         role = get(message.guild.roles, id=c.ROLES[emoji])
-        admin_role = get(message.guild.roles, id=c.EXCROLE['👨‍💻'])
+        admin_role = get(message.guild.roles, id=c.EXCROLE['admin'])
         
         if admin_role in member.roles and len(member.roles) <= c.MAX_ROLES_PER_USER:
             await member.add_roles(role)
